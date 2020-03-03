7e5e455976378b1b94b098aa69b6fe851cab8f6a
sanket naik <sanketn7941@gmail.com>
2020-03-03 17:17:41 +0530

    new example added :- DUT on and off

---
+++ Example/multiple Start_Stop.py
@@ -0,0 +1,31 @@
+import pyvisa as visa
+from pyinstrument import PSupply
+
+rm = visa.ResourceManager()
+rm.list_resources()
+print(rm.list_resources()[0])
+PS = rm.open_resource(rm.list_resources()[0])   # choose the proper address for your instrument
+print('Power supply detected=> ' + PS.query('*IDN?'))  # chk communication is established or NOT
+
+ps = PSupply(PS)
+
+maxVoltage = 10     # set max voltage limit
+minVoltage = 0      # set Min voltage limit
+delaySec = 2        # set delay time between on and off
+
+# turn on Power supply
+ps.on()
+# set max current limit
+ps.setCurr(3)
+
+for i in range(10):
+    ps.setVolt(maxVoltage)      # set max voltage
+    ps.delay(delaySec)          # delay
+    ps.setVolt(minVoltage)      # Set min voltage
+    ps.delay(delaySec)          # delay
+
+ps.off()
+PS.close()
+rm.close()
+
+print("process complete :)")
