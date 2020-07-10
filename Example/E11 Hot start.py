e4559896bc285fb5cb95a651c34c1762e3ba52f1
sourabh1994 <shetesourabh1994@gmail.com>
2020-07-10 16:19:27 +0530

    Update E11 Hot start.py

--- Example/E11 Hot start.py
+++ Example/E11 Hot start.py
@@ -1,7 +1,17 @@
 """
 
-sshete requirment for VW216 E09.2
+E-11 Start pulses
 
+Purpose
+When the engine is started, the battery voltage drops to a low value for a short period in
+order to then rise again slightly. Most components are briefly activated immediately
+before the starting process, deactivated during the starting process, and then activated
+again after the starting process once the engine is running. This test examines the
+behavior of the component in the event of voltage sags caused by starting.
+The starting process may be carried out under varying vehicle starting conditions: cold
+start and hot start (automatic restart in the case of a start-stop system). In order to cover
+both cases, two different test cases are required. A component must always run through
+both sequences.
 
 """
 import pyvisa as visa
